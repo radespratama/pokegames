8ec782041744fa2f037c3c6e2d07ba70c00ad29c
radespratama <puturades89@gmail.com>
2023-01-15 10:31:48 +0800

    update vite version and add vite plugin swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, splitVendorChunkPlugin, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
