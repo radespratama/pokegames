4605d3ccd4d6296a6813dd08baef664abeeeb270
radespratama <puturades89@gmail.com>
2025-12-25 08:39:44 +0800

    refactor: cleanup unused type icons and enable dynamic API paths

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,2 +1,4 @@
 export const POKEMON_API = import.meta.env.VITE_POKEMON_API || "";
 export const POKEMON_IMAGE = import.meta.env.VITE_POKEMON_IMAGE || "";
+export const VITE_POKEMON_TYPE_ICON =
+  import.meta.env.VITE_POKEMON_TYPE_ICON || "";
